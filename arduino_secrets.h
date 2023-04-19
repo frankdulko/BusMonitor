e951e6f4c71f65f3a5b9b2b97a1f0df653554132
frankdulko <79063207+frankdulko@users.noreply.github.com>
2023-04-19 09:44:14 -0400

    Delete arduino_secrets.h

--- arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-//  #define SECRET_SSID "sandbox370"    // network name
-//  #define SECRET_PASS "+s0a+s03!2gether?"    // network password
-   #define SECRET_SSID "SpectrumSetup-B9"    // network name
-   #define SECRET_PASS "drivelama53"    // network password
-   const String  KEY = "019d0445-37d4-4549-8714-47c3e5ea9ba7";
